e365c784b2b29b908fa34856c0f755d060fb505a
Paulo Vítor Araújo <33043117+araujovitorpaulo@users.noreply.github.com>
2024-04-02 16:10:23 -0300

    migrations + applications.properties changes

---
+++ src/main/resources/db/migration/V2__create-table-alunos.sql
@@ -0,0 +1,17 @@
+create sequence alunos_seq start with 1 increment by 1;
+
+create table alunos(
+    id number default alunos_seq.nextval,
+    nome varchar(100) not null,
+    idade int not null,
+    email varchar(100) not null unique,
+    telefone varchar(10) not null unique,
+    sexo varchar(20) not null,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(10) not null,
+    cidade varchar(100) not null,
+    uf char(2) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    primary key(id));
