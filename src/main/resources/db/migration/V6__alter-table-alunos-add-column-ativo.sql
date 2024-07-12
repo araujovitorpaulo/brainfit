f8c92b3b19234fc1ee3f06c59caf43f1f3957e90
Paulo Vítor Araújo <33043117+araujovitorpaulo@users.noreply.github.com>
2024-07-12 20:58:58 -0300

    [fix]: Exclusão de migrations repetidas

--- src/main/resources/db/migration/V6__alter-table-alunos-add-column-ativo.sql
+++
@@ -1,3 +0,0 @@
-alter table alunos add ativo char(1) default 'S' check (ativo in('S','N'));
-
-update alunos set ativo='S';
--- target/classes/db/migration/V5__alter-table-instrutores-add-column-ativo.sql
+++
@@ -1,3 +0,0 @@
-alter table instrutores add ativo char(1) default 'S' check (ativo in('S','N'));
-
-update instrutores set ativo='S';
